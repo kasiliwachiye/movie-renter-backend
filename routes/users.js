3ac350c32edbc6bc72811f1f3f20f5455e405988
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-09-06 14:41:00 +0300

    chore: hash passwords

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const { User, Validator } = require("../models/user");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 
@@ -21,6 +22,8 @@ router.post("/", async (req, res) => {
   // });
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password,salt)
 
   await user.save();
 
